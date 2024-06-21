d24dd5937986b37054e3fe3a0af3b61419358e89
Ashan Wijebandara <wijebandara.ashan@gmail.com>
2024-06-21 20:07:54 +0530

    initialize next app

---
+++ salary-calculator/app/users/page.tsx
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+import React from 'react'
+
+ const UserPage = () => {
+  return (
+    <div><h1>Hello WW</h1>
+    <Link href="users/new"> User </Link></div>
+  )
+}
+
+export default UserPage;
